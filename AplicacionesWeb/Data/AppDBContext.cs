c13fb03ca915b051600ff145c94601148625697f
LuisS-byte <luis.sarmiento@catolica.edu.sv>
2025-02-13 18:51:49 -0600

    P

---
+++ AplicacionesWeb/Data/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AplicacionesWeb.Data
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+
+    }
+}
